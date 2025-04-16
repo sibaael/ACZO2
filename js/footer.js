1fab0a1a642941f8669b79a6d9751c75cd994054
sibaael <siba.ael@gmail.com>
2025-04-15 22:48:40 -0300

    actualizaciones varias

---
+++ js/footer.js
@@ -0,0 +1,22 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+
+	setFooter();
+
+
+	function setFooter() {
+		$("#footerdiv").load("../footer.html"); 
+	}
+
+});
