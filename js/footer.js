01f0e0f83604ca06fb5afa8c740599fa847ee604
sibaael <siba.ael@gmail.com>
2025-04-26 22:46:26 -0300

    ROOT

--- js/footer.js
+++ js/footer.js
@@ -11,16 +11,15 @@ $(document).ready(function()
 {
 	"use strict";
 
-
+	const subfolder = "/ACZO2";
+	var rootSite = window.location.origin + subfolder;
+	// var rootSite = window.location.origin;
+	console.log(rootSite);
 	setFooter();
 
 
 	function setFooter() {
-		var subdomain = window.location.hostname.split('/')[0];
-		if (subdomain === 'www' || subdomain === window.location.hostname) {
-			subdomain = ''; // or handle the case where there's no subdomain
-		}
-		$("#footerdiv").load(subdomain + "/footer.html"); 
+		$("#footerdiv").load(rootSite + "/footer.html"); 
 	}
 
 });
--- js/menu.js
+++ js/menu.js
@@ -11,16 +11,15 @@ $(document).ready(function()
 {
 	"use strict";
 
-
+	const subfolder = "/ACZO2";
+	var rootSite = window.location.origin + subfolder;
+	// var rootSite = window.location.origin;
+	console.log(rootSite);
 	setMenu();
 
 
 	function setMenu() {
-		var subdomain = window.location.hostname.split('/')[0];
-		if (subdomain === 'www' || subdomain === window.location.hostname) {
-			subdomain = ''; // or handle the case where there's no subdomain
-		}
-		$("#menudiv").load(subdomain + "/menu.html"); 
+		$("#menudiv").load(rootSite + "/menu.html"); 
 	}
 
 });
